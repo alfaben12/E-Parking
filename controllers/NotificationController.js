bb8bc793ce3295dda85471f49642e486e435e014
Thariqalfa <zrav420@gmail.com>
2019-08-13 15:02:39 +0700

    update notification

--- controllers/NotificationController.js
+++ controllers/NotificationController.js
@@ -11,7 +11,7 @@ module.exports = {
             "to": token,
             "notification": {
                 "title": "E-Parkir",
-                "body": "Pembayaran diterima Rp."+ nominal + " pada "+ moment().format('dddd, MMMM Do YYYY') +".",
+                "body": "Pembayaran diterima Rp."+ nominal + " dari "+ vehicle_registration +" pada "+ moment().format('dddd, MMMM Do YYYY') +".",
                 "sound": "default"
             }
         };
