25822018124bc0600515813587cce41eda78ffc2
Thariqalfa <zrav420@gmail.com>
2019-08-16 17:17:13 +0700

    update fix cors

--- server.js
+++ server.js
@@ -14,6 +14,20 @@ const ParkingTypeRouter = require('./routes/ParkingTypeRouter');
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// allow request
+app.use(cors());
+app.use(function(req, res, next) {
+	res.header('Access-Control-Allow-Origin', '*');
+	res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH,OPTIONS');
+	res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+	// allow preflight
+	if (req.method === 'OPTIONS') {
+		res.send(200);
+	} else {
+		next();
+	}
+});
+
 app.get('/', (req, res) => res.send('Hello KMIPN 2019!'))
 app.use('/accounts', AccountRouter);
 app.use('/logins', LoginRouter);
